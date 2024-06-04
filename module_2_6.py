adf51ad1cae3707e2125e37c0c37baa3575c21ae
world-11 <temavetrov1@mail.ru>
2024-06-04 23:34:22 +0300

    Add files via upload

--- module_2_6.py
+++ module_2_6.py
@@ -8,6 +8,9 @@ def single_root_words(root_word, *other_words):
             same_words.append(other_words[i])
         elif len(m) > len(n) and m.count(n):
             same_words.append(other_words[i])
-    print(same_words)
+    return same_words
 
-single_root_words('Samara', 'sam', 'ara', 'dad', 'well')
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
