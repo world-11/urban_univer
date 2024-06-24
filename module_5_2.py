fe686b4c661bbb6f3a494634c69785e8e42923d7
world-11 <temavetrov1@mail.ru>
2024-06-25 00:30:45 +0300

    Add files via upload

--- module_5_2.py
+++ module_5_2.py
@@ -2,8 +2,9 @@ class House:
     def __init__(self, numberOfFloors):
         self.numberOfFloors = 0
 
-    def setNewNumberOfFloors(floors):
+    def setNewNumberOfFloors(self, floors):
         House.numberOfFloors = floors
         print(House.numberOfFloors)
 
-h1 = House.setNewNumberOfFloors(9)
+h1 = House(0)
+h1.setNewNumberOfFloors(9)
