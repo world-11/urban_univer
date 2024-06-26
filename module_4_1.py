2bb11a72cd652f03cb0bc952e29b37e9b1829be5
world-11 <temavetrov1@mail.ru>
2024-06-26 20:20:17 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import fake_divide as fd
+from  true_math import true_divide as td
+
+
+result1 = fd(69, 3)
+result2 = fd(3, 0)
+result3 = td(49, 7)
+result4 = td(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
