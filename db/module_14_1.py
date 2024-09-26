66cd0a4600d17a3eedb91b88bbc52ffe98568915
world-11 <temavetrov1@mail.ru>
2024-09-26 20:51:09 +0300

    Add files via upload

---
+++ db/module_14_1.py
@@ -0,0 +1,52 @@
+import sqlite3
+
+connection =  sqlite3.connect('database.db')
+cursor = connection.cursor()
+
+# удаление таблицы
+# cursor.execute(('''DROP TABLE Users'''))
+
+# создание таблицы
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+# создание индекса
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users(email)")
+
+# удаление записей
+# cursor.execute("DELETE FROM Users WHERE balance = ?", (1000,))
+
+# добавление записей
+# for i in range(10):
+#     cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+#                    (f'user{i+1}', f'example{i+1}@gmail.com', (i+1)*10, 1000))
+
+#обновление баланса
+# i = 1
+# while i <= 10:
+#  cursor.execute("UPDATE Users SET balance = ? WHERE id = ?",
+#                    (500, f'{i}'))
+#  i += 2
+
+#удаление каждой 3 записи, начиная с 1
+# i = 1
+# while i <= 10:
+#  cursor.execute("DELETE FROM Users WHERE id = ?",
+#                    (f'{i}',))
+#  i += 3
+
+#выборка
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
+users = cursor.fetchall()
+for user in users:
+ print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+
+connection.commit()
+connection.close()
