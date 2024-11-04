4064cf4d69f7f5de99a903434f0644c971e02800
world-11 <temavetrov1@mail.ru>
2024-11-04 20:34:29 +0300

    Update db.py

--- Unit_17/alembic/app/backend/db.py
+++ Unit_17/alembic/app/backend/db.py
@@ -1,1 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import Column, Integer, String
 
+engine = create_engine('sqlite:///taskmanager.db')
+
+SessionLocal = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    pass
