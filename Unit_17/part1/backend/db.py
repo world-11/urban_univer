4a4b20dc19dbc87d4403d0acf5b43d7cf9a59916
world-11 <temavetrov1@mail.ru>
2024-11-08 11:57:53 +0300

    Add files via upload

--- Unit_17/part1/backend/db.py
+++ Unit_17/part1/backend/db.py
@@ -1,1 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
+engine = create_engine('sqlite:///taskmanager.db')
+
+SessionLocal = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    pass
